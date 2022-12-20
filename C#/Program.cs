b7c506395f88dddb7a98b01139b6e329aa5b44c7
NaumovaKate <naumova.katrine@gmail.com>
2022-12-20 21:16:20 +0300

    сохранение

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+// int number = new Random().Next(10,100);
+// Console.WriteLine(number);
+// if (number/10 > number%10)
+// {
+//     Console.WriteLine(number/10);
+// }
+// else
+// {
+//     Console.WriteLine(number%10);
+// }
+
+ 
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+// if (number/1000 > number%1000)
+// {
+//     Console.WriteLine(number/100);
+//     }
+// else
+// {
+//     Console.WriteLine(number%100);
+// }
