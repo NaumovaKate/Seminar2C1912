14cceb5dc57470b3f9554468e32b8d10b42b80fe
NaumovaKate <naumova.katrine@gmail.com>
2023-01-07 11:00:01 +0300

    Дз решено кроме первой

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -1,18 +1,25 @@
-﻿Задача 19
+﻿// Задача 19
 
-Напишите программу, которая принимает на вход пятизначное число и проверяет, 
-является ли оно палиндромом.
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
 
-14212 -> нет
+// 14212 -> нет
 
-12821 -> да
+// 12821 -> да
 
-23432 -> да
+// 23432 -> да
 
-Console.WriteLine("Введите 5-ти значное число: ");
-int num = new Random().Next(10000,100000);
-string stringNumber = Convert.ToString(number);
-if 
+// Console.WriteLine("Введите 5-ти значное число: ");
+// int i = new Random().Next(10000, 100000);
+// int num = Convert.ToInt32(Console.ReadLine());
+// if ((num[0]==num[4]) && (num[1]==num[3]))
+// {
+//     Console.WriteLine($"Число является палиндромом");
+// }
+// else
+//      {
+//          Console.WriteLine($"Число не является палиндромом");
+//      }
 
 
 // Задача 21
@@ -24,6 +31,25 @@ if
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+// Console.Clear();
+// Console.Write("Введите X1: ");
+// int x1=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y1: ");
+// int y1=int.Parse(Console.ReadLine());
+// Console.Write("Введите Z1: ");
+// int z1=int.Parse(Console.ReadLine());
+// Console.Write("Введите X2: ");
+// int x2=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y2: ");
+// int y2=int.Parse(Console.ReadLine());
+// Console.Write("Введите Z2: ");
+// int z2=int.Parse(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2));
+
+// Console.WriteLine($"d={d:f2}");
+
+
 // Задача 23
 
 // Напишите программу, которая принимает на вход число (N) 
@@ -31,3 +57,15 @@ if
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+int N = new Random().Next(1,100);
+Console.WriteLine(N);
+
+// Console.Write("Введите число N: ");
+// int N = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= N; i++)
+{
+    Console.Write($"Таблица{i*i*i} ");
+}
